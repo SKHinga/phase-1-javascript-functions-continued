ff081b5e1889deaf82376b167e79ed95925e22a3
Samuel Hinga <samuelhinga200@gmail.com>
2022-04-10 02:48:36 +0300

    Sc: functions scope

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun (activity = 'roller-skate'){
+  return `This Saturday, I want to ${activity}!`
+}
+saturdayFun('bathe my dog');
+function mondayWork (doing = 'go to the office') {
+  return `This Monday, I will ${doing}.`
+}
+doing('work from home');
+
+function wrapAdjective(result = '*'){
+  return function emphatic (one = 'special'){
+    return `You are ${result}${one}${result}!`
+  }
+};
+wrapAdjective()('a hard worker');
+wrapAdjective('||')('a dedicated programmer');
